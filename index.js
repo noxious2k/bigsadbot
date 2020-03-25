639d248cfc432021b7307bf8657e525096aa5986
noxious2k <62644132+noxious2k@users.noreply.github.com>
2020-03-25 09:19:53 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+client.on('message', (message) => {
+
+    if(message.content == '!play sketchers') {
+        message.reply('shut the fuck up');
+    }
+
+});
+
+bot.login('NjkyMjgxODgzMjg2OTYyMTk2.XnsQ7w.Lclvc6IN8F16l8UQL8SRVvOtx1s');
+
+
+
+
